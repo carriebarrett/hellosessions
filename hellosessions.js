af30894b15a5ac238c040f25bcb068919d0eb966
Carrie Barrett <67287344+carriebarrett@users.noreply.github.com>
2021-02-27 17:13:53 -0800

    Update hellosessions.js

--- hellosessions.js
+++ hellosessions.js
@@ -11,8 +11,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({
 	secret:'DefinitelySecret'
-	resave: true,
-    saveUninitialized: true
 }));
 
 app.engine('handlebars', handlebars.engine);
